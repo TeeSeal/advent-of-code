aca2599383b5e7326360fc8472c09d20e4630990
TeeSeal <teesealz@gmail.com>
2018-12-03 15:24:22 +0200

    Day 3 Part 2

--- 2018/03/main.c
+++ 2018/03/main.c
@@ -36,24 +36,51 @@ void overlapping_points(struct Claim c1, struct Claim c2, struct Point **points,
 	}
 }
 
+int point_in_claim(struct Point point, struct Claim claim) {
+	return
+		point.x >= claim.x &&
+		point.x < claim.x + claim.width &&
+		point.y >= claim.y &&
+		point.y < claim.y + claim.height;
+}
+
+struct Claim* find_non_overlapping_claim(struct Claim *claims, int claim_count, struct Point *points, int point_count) {
+	int found_point;
+
+	for (int i = 0; i < claim_count; i++) {
+		found_point = 0;
+
+		for (int j = 0; j < point_count; j++) {
+			if (point_in_claim(points[j], claims[i])) {
+				found_point = 1;
+				break;
+			}
+		}
+
+		if (!found_point) return &claims[i];
+	}
+
+	return NULL;
+}
+
 int main() {
 	FILE* input;
 	input = fopen("input.txt", "r");
 
-	int k = 0;
+	int claim_count = 0;
 	struct Claim *claims;
 
-	for (;;k++) {
-		claims = realloc(claims, (k + 1) * sizeof(struct Claim));
+	for (;;claim_count++) {
+		claims = realloc(claims, (claim_count + 1) * sizeof(struct Claim));
 
 		if (
 			fscanf(
 				input, "#%d @ %d,%d: %dx%d\n",
-				&claims[k].id,
-				&claims[k].x,
-				&claims[k].y,
-				&claims[k].width,
-				&claims[k].height
+				&claims[claim_count].id,
+				&claims[claim_count].x,
+				&claims[claim_count].y,
+				&claims[claim_count].width,
+				&claims[claim_count].height
 			) == EOF
 		) break;
 	}
@@ -61,12 +88,16 @@ int main() {
 	int point_count = 0;
 	struct Point *points;
 
-	for (int i = 0; i < k - 1; i++) {
-		for (int j = i + 1; j < k; j++) {
+	for (int i = 0; i < claim_count - 1; i++) {
+		for (int j = i + 1; j < claim_count; j++) {
 			overlapping_points(claims[i], claims[j], &points, &point_count);
 		}
 	}
 
-	printf("Total: %d\n", point_count);
+	printf("Overlapping square inches: %d\n", point_count);
+
+	struct Claim *non_overlapping_claim = find_non_overlapping_claim(claims, claim_count, points, point_count);
+
+	printf("ID of claim that doesn't overlap: %d\n", (*non_overlapping_claim).id);
 	return 0;
 }
