e982ba68c70b6f2447e814aa7cdbcad16635eef4
TeeSeal <teesealz@gmail.com>
2018-12-15 01:17:45 +0200

    Day 12 part 2

--- 2018/12_clj/main.clj
+++ 2018/12_clj/main.clj
@@ -19,9 +19,20 @@
           (pattern-at idx (str ".." x "..")) "."))
           (str ".." x ".."))))
 
-(def final
-  (nth (iterate next-gen pots) 20))
-
-(println
+(defn gen-sum [pots, gen]
   (reduce +
-    (map-indexed (fn [idx itm] (if (= itm \#) (- idx 40) 0)) final)))
+    (map-indexed
+      (fn [idx itm] (if (= itm \#) (- idx (* gen 2)) 0))
+      pots)))
+
+(defn evolve [init, max]
+  (loop [gen 0 pots init previous-sum 0]
+    (def current-sum (gen-sum pots gen))
+    (println (str gen ": " current-sum " " (- current-sum previous-sum)))
+    (if (< gen max)
+      (recur (inc gen) (next-gen pots) current-sum))))
+
+(evolve pots 200)
+; On generation 99 the difference converges to +25 per generation
+; Value at genreation 98: 3441
+; 3441 + (50000000000 - 98) * 25 = 1,250,000,000,991
